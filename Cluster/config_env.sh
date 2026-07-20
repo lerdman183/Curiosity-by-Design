83738e57c1c1fdcd29056f4b7e12cf772c4d73d1
lerdman183 <lerdman@ualberta.ca>
2026-07-20 14:58:07 -0600

    Troubleshooting sh files

--- Cluster/config_env.sh
+++ Cluster/config_env.sh
@@ -13,6 +13,23 @@ source $SLURM_TMPDIR/ENV/bin/activate
 
 pip install --upgrade pip --no-index
 
-pip install --no-index torch transformers peft datasets wandb huggingface_hub deepspeed typing_extensions
+pip install --no-index typing_extensions
+
+pip install --no-index torch transformers peft datasets wandb huggingface_hub deepspeed
+
+echo "Python:"
+which python
+
+echo "Pip:"
+which pip
+
+echo "Torchrun:"
+which torchrun
+
+python -m pip show typing_extensions
+python -m pip show torch
+
+python -c "import typing_extensions; print('typing_extensions OK')"
+python -c "import torch; print(torch.__version__)"
 
 echo "Done installing virtualenv!"
--- Cluster/launch_training_deepspeed.sh
+++ Cluster/launch_training_deepspeed.sh
@@ -8,9 +8,9 @@ echo "r$SLURM_NODEID master: $HEAD_NODE"
 echo "r$SLURM_NODEID Launching python script"
 
 torchrun \
---nnodes $SLURM_NNODES \
---nproc_per_node 2 \ 
---rdzv_backend c10d \
---rdzv_endpoint "$HEAD_NODE" \
+--nnodes=$SLURM_NNODES \
+--nproc_per_node=2 \
+--rdzv_backend=c10d \
+--rdzv_endpoint="$HEAD_NODE" \
 Code/supplementary-material/clarification_module.py \
 --deepspeed_config="./ds_config.json"
