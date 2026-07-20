ee8af4d3cef937bcdca8997db65e88c01616357f
lerdman183 <lerdman@ualberta.ca>
2026-07-20 17:38:16 -0600

    Changed job scripts to fix issues with launching across multiple nodes

--- Cluster/launch_training_deepspeed.sh
+++ Cluster/launch_training_deepspeed.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 # Code adopted from https://docs.alliancecan.ca/wiki/Deepspeed
 
-source $SLURM_TMPDIR/ENV/bin/activate
+module purge
+module load python gcc arrow/21.0.0 cuda
 
-echo "Installing typing_extensions =================="
-pip install --no-index typing_extensions
-pip install --no-index typing-extensions
+source ~/Curiosity-by-Design/venv/bin/activate
 
 export NCCL_ASYNC_ERROR_HANDLING=1
+# Force NCCL to use the correct network interface if needed (Alliance cluster standard)
+export NCCL_IB_DISABLE=0
 
 echo "r$SLURM_NODEID master: $HEAD_NODE"
 echo "r$SLURM_NODEID Launching python script"
@@ -16,6 +17,7 @@ torchrun \
 --nnodes=$SLURM_NNODES \
 --nproc_per_node=2 \
 --rdzv_backend=c10d \
+--rdzv_id=$SLURM_JOB_ID \
 --rdzv_endpoint="$HEAD_NODE" \
 Code/supplementary-material/clarification_module.py \
 --deepspeed_config="./ds_config.json"
